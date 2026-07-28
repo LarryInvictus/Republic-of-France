303ff932f3ab73ee49b9df16d5b7acad51613b86
LarryInvictus <recklesslythe1@gmail.com>
2026-07-28 16:29:34 -0400

    Remove event listener for join button

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const joinBtn = document.getElementById("joinFranceBtn");
 
-  joinBtn.addEventListener("click", () => {
-    window.location.href = "https://discord.gg/cJJ8vG6Kk";
-  });
-});
